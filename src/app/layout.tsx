4baaf9cecb7e1bcfb1c77db2fb1de312692a303f
cavesdev <24899540+cavesdev@users.noreply.github.com>
2025-03-15 15:59:49 -0600

    chore: fix eslint and remove unused boilerplate code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,4 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
 import { Provider } from '@/components/ui/provider';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
-});
 
 export default function RootLayout({
   children,
@@ -20,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Provider>
           {children}
         </Provider>
